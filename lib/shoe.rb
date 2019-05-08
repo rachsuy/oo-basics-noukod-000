439257e72330338515bb47ba6a82fe68c6605d59
Rachele Suy <rachelesuy@gmail.com>
2019-05-08 15:07:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size
+  attr_reader :new
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
