9abd40d85711ea3f017e64b9225955bb96e82d14
Rachele Suy <rachelesuy@gmail.com>
2019-05-08 15:02:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,6 +10,18 @@ end
  def author=(author)
     @author = author
   end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
 #Book.new("And Then There Were None")
 end
 
